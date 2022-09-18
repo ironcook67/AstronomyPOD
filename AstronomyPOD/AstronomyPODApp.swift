b7a45952685c6e667516065c4c39d11354bde1e6
Chon Torres <ironcook67@gmail.com>
2022-09-17 22:31:33 -0700

    Initial Commit

---
+++ AstronomyPOD/AstronomyPODApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronomyPODApp.swift
+//  AstronomyPOD
+//
+//  Created by Chon Torres on 9/17/22.
+//
+
+import SwiftUI
+
+@main
+struct AstronomyPODApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
